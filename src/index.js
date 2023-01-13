f01bd5044a854d49b7e842a4c5e896f4946d6564
javisan81 <javisan_81@hotmail.com>
2023-01-13 14:33:57 +0100

    Inicio de la app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let gitHubUrl='https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+function App() {
+      const [userData, setUserData] = useState([]);
+
+      useEffect(() => {
+        getGitHubUserWithFetch();
+      }, []);
+
+     
+      const getGitHubUserWithFetch = async () => {
+        const response = await fetch(gitHubUrl);
+        const jsonData = await response.json();
+        setUserData(jsonData.feed);
+        
+      };
+
+      
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h2>Podcasts list</h2>
+          </header>
+          {
+          	console.log(userData)
+          	
+          }
+        </div>
+      );
+    }
+
+    
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
